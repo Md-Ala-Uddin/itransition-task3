5084badb699ef27b6729271e668898cef9246e69
MD ALA UDDIN <pial469048@gmail.com>
2025-06-08 23:12:46 +0600

    feat: add check for duplicate dice

--- src/dice_parser.ts
+++ src/dice_parser.ts
@@ -5,6 +5,11 @@ export class DiceParser {
         const diceArgs = args.slice(2);
         if (diceArgs.length < 3) throw new Error("Atleast 3 dice are required");
         
+        if (haveDuplicates(diceArgs))
+            throw new Error(
+                "Duplicate dice detected, dice can not be identical and need to be non-transitive."
+            );
+
         return diceArgs.map((arg, i) => {
             const parts = arg.split(",").map(Number);
             if (parts.length !== 6 || parts.some(isNaN))
@@ -13,5 +18,15 @@ export class DiceParser {
 
             return new Dice(parts);
         });
+    }
+}
+
+function haveDuplicates(diceArgs: string[]) {
+    for (let i = 0; i < diceArgs.length; i++) {
+        for (let j = i + 1; j < diceArgs.length; j++) {
+            if (diceArgs[i] === diceArgs[j]) {
+                return true;
+            }
+        }
     }
 }
